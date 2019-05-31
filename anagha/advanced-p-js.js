dbf5a25619c56bc769d6289c59f45363ccfcb34f
bhatanagha <49675714+bhatanagha@users.noreply.github.com>
2019-05-31 11:22:35 +0530

    Component training Anagha (#1)
    * added comment
    
    * added folder for anagha

---
+++ anagha/advanced-p-js.js
@@ -0,0 +1,12 @@
+class AdvancedP extends HTMLElement {
+    connectedCallback() {
+        this._build()
+    }
+
+    _build() {
+        
+    }
+}
+
+window.customElements.define('advanced-p', AdvancedP)
+
