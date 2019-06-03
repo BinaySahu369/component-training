44aa2c6392c76aa571e235ced8867da417e94c67
nagamalleshavenkategowda <49677363+nagamalleshavenkategowda@users.noreply.github.com>
2019-06-03 16:01:13 +0530

    renamed js file

--- nagamallesh/advanced-p.js
+++ nagamallesh/advanced-p.js
@@ -1,16 +1,19 @@
-function findTrigger (that) {
-    const trigger = document.querySelectorAll('[advancedTag]')
-    for (let i = 0; i < trigger.length; i++) {
-        if ( trigger[i] && that.getAttribute('id')===trigger[i].getAttribute('advancedTag')) {
-            trigger[i].addEventListener('click', () => {
-             that.classList.toggle('animatePara')
-            })
-         }
+function findTrigger (that,tagPresent) {
+    if(tagPresent) {
+        const trigger = document.querySelectorAll('[advancedTag]')
+        for (let i = 0; i < trigger.length; i++) {
+            if ( trigger[i] && that.getAttribute('id')===trigger[i].getAttribute('advancedTag')) {
+                trigger[i].addEventListener('click', () => {
+                 that.classList.toggle('animatePara')
+                })
+             }
+        } 
     }
-
+   
 }
 
 class AdvancedP extends HTMLElement {
+    
     connectedCallback () {
         this._build()
     }
@@ -22,12 +25,12 @@ class AdvancedP extends HTMLElement {
         if (this.hasAttribute('color')) {
             this.style.color=this.getAttribute('color')
         }
-        if (this.hasAttribute('bold')) {
-        const tagType = this.getAttribute('type');
-        const validTextTags = ["div", "h1", "h2","h3","h4","h5","h6","span","p"];
         let tagPresent =false;
+        if (this.hasAttribute('type')) {
+        const tagType = this.getAttribute('type');
+        const validTextTags = ['div', 'h1', 'h2','h3','h4','h5','h6','span','p']
         for (var tag in validTextTags) {
-           if (validTextTags[tag] === tagType) {
+           if (validTextTags[tag] === tagType.trim().toLowerCase()) {
             tagPresent =true;           
             break;
            }
@@ -40,16 +43,16 @@ class AdvancedP extends HTMLElement {
             this.appendChild(newElement)
           }
           else {
-            const newElement=document.createElement('span')
-            const text = tagType+' is not a HTML element'
+            const newElement=document.createElement('div')
+            newElement.classList.add('invalid')
+            const text = tagType + ' is not a HTML element'
             newElement.textContent = text
-            newElement.style.color='red'
-              this.textContent = ''
-              this.parentElement.appendChild(newElement)
+            this.textContent = ''
+            this.appendChild(newElement)
           }
         }
           
-          findTrigger(this)
+          findTrigger(this,tagPresent)
     }
 }
 window.customElements.define('advanced-p', AdvancedP)
